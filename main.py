0dd4d6ad9caf0e6a7b0f3d1805c4849c6969de56
DanielKorsah <dkorsah15@aol.co.uk>
2019-11-15 17:10:04 +0000

    Start of connection scanning

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-
 from numpy import genfromtxt
 
 
-def FileImport():
+def main():
     class Node:
-        def __init__(self, visited, nodeNumber, x, y):
+        def __init__(self, nodeNumber, visited, x, y, tentDistance):
             self.nodeNumber = nodeNumber
             self.visited = visited
             self.x = x
             self.y = y
+            self.tentDistance = tentDistance
 
-    text_file = genfromtxt("generated1000-1.cav", delimiter=',')
+    text_file = genfromtxt("generated30-1.cav", delimiter=',')
 
     N = int(text_file[0])
     nodeList = []
@@ -18,16 +18,27 @@ def FileImport():
 
     for a in range(N):
         column = []
-        nodeList.append(Node(int(a), bool(False), int(text_file[a * 2 + 1]), int(text_file[a * 2 + 2])))
+        nodeList.append(Node(int(a), bool(False), int(text_file[a * 2 + 1]), int(text_file[a * 2 + 2]), -1))
         for b in range(N):
             column.append(text_file[a + b + 2 * N + 1])
 
         nodeMatrix.append(column)
 
+    nodeList[0].tentDistance = 0
+    firstNode = nodeList[0]
+    destination = nodeList[-1]
+    connectedTo = []
 
-def main():
-    FileImport()
+    currentNode = firstNode
+    unvisitedNodes = nodeList
+    for b in range(len(nodeMatrix)):
+        # print(nodeMatrix[currentNode.nodeNumber][b])
+        if nodeMatrix[currentNode.nodeNumber][b] == 1.0:
+            connectedTo.append(nodeList[b])
+    for a in range(len(nodeMatrix)):
+        print(nodeMatrix[a])
+
+    print(connectedTo)
 
 
 main()
-
